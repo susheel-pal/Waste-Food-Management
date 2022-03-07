3c08a831a7476e09e6a7ac1df52061b21ddf6f0d
susheel pal <susheelpal057@gmail.com>
2022-03-07 18:08:59 +0530

    app completed

---
+++ app/src/main/java/com/example/wastefoodmanagement/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.example.wastefoodmanagement;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+@Database(entities = {Task.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract TaskDao taskDao();
+}
